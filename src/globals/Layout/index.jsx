ef701c50d6a6b6c851e8ebab9f2eaa2abecdab49
Trevor Jarrett <tjarrett@trendyminds.com>
2020-09-25 17:28:11 -0400

    some updates and tailwind config

--- src/globals/Layout/index.jsx
+++ src/globals/Layout/index.jsx
@@ -8,13 +8,24 @@ const Layout = ({ children }) => (
   <>
     <div className="max-w-screen-xl w-full h-full flex">
       <div className="w-full max-w-xs relative">
-        <div className="bg-black w-40 h-40 ml-auto mr-auto"></div>
-        <div className="bg-black w-1 h-screen ml-auto mr-auto absolute left-0 right-0 z-0"></div>
-        <div className="m-20 w-40 ml-auto mr-auto relative">
-          <ul className="text-center">
-            <li className="p-2 bg-yellow-100 mb-5">Link</li>
-            <li className="p-2 bg-yellow-100 mb-5">Link</li>
-            <li className="p-2 bg-yellow-100">Link</li>
+        <div className="w-40 h-40 ml-auto mr-auto">
+          <img className="rounded-full shadow-circle border-2 border-royalBlue" src="https://avatars3.githubusercontent.com/u/3466483?s=460&u=6ccad09f09f771806a19bb77dd4718b1868496bb&v=4" alt="It's me; Trevor Jarrett"/>
+        </div>
+        <div className="bg-navyBlue w-1 h-screen ml-auto mr-auto absolute left-0 right-0 z-0"></div>
+        <div className="m-10 w-56 ml-auto mr-auto relative">
+          <div className="mt-10 pb-2 pt-2 text-center bg-royalBlue">
+            <h1 className="text-ulthuanGrey">Trevor Jarrett</h1>
+          </div>
+          <ul className="mt-10 text-center">
+            <li className="p-2 mb-5 bg-royalBlue">
+              <a className="text-ulthuanGrey" href="#">Link</a>
+            </li>
+            <li className="p-2 mb-5 bg-royalBlue">
+              <a className="text-ulthuanGrey" href="#">Link</a>
+            </li>
+            <li className="p-2 mb-5 bg-royalBlue">
+              <a className="text-ulthuanGrey" href="#">Link</a>
+            </li>
           </ul>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   theme: {
-    extend: {},
+    colors: {
+      navyBlue: '#1e3d58',
+      royalBlue: '#057dcd',
+      blueGrotto: '#43b0f1',
+      ulthuanGrey: '#e8eef1'
+    },
+    bodrerColor: theme => ({
+      "royalBlue": '#057dcd'
+    })
   },
   variants: {},
   plugins: [],
